2d5a33dd9bdda4f0f5ff02bac7c25ffef05f6dbd
IKStreamIvo <ivostreaming@gmail.com>
2018-01-27 00:52:20 +0100

    Gamemanager!!!!!!!!!!

---
+++ Assets/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Ship1Prefab;
+    public GameObject Ship2Prefab;
+    public Vector2 Ship1Spawn;
+    public Vector2 Ship2Spawn;
+    public GameObject ShipTetherPrefab;
+
+    public float moveSpeed = 3f;
+    public float rotateSpeed = 3f;
+
+    public Ship ship1;
+    public Ship ship2;
+    public ShipTether tether;
+
+    private Vector2 p1movement;
+    private Vector2 p2movement;
+    private Vector2 p1rotation;
+    private Vector2 p2rotation;
+
+    void Start ()
+    {
+        //Spawn players	
+        ship1 = Instantiate(Ship1Prefab, Ship1Spawn, Quaternion.identity).GetComponent<Ship>();
+        ship2 = Instantiate(Ship2Prefab, Ship2Spawn, Quaternion.identity).GetComponent<Ship>();
+
+        //Spawn tether
+        tether = Instantiate(ShipTetherPrefab).GetComponent<ShipTether>();
+
+        tether.ship1 = ship1;
+        tether.ship2 = ship2;
+    }
+	
+	void Update ()
+    {
+        p1movement = new Vector2(Input.GetAxisRaw("P1MovHor"), Input.GetAxisRaw("P1MovVer"));
+        p2movement = new Vector2(Input.GetAxisRaw("P2MovHor"), Input.GetAxisRaw("P2MovVer"));
+
+        p1rotation = new Vector2(Input.GetAxisRaw("P1RotHor"), Input.GetAxisRaw("P1RotVer"));
+        p2rotation = new Vector2(Input.GetAxisRaw("P2RotHor"), Input.GetAxisRaw("P2RotVer"));
+    }
+
+    private void FixedUpdate()
+    {
+        ship1.rb.velocity = p1movement.normalized * moveSpeed;
+        ship2.rb.velocity = p2movement.normalized * moveSpeed;
+
+        if (p1rotation.x != 0f || p1rotation.y != 0f)
+        {
+            float angley = Mathf.Atan2(p1rotation.y, p1rotation.x) * Mathf.Rad2Deg;
+            angley = (angley + 360f) % 360f;
+            float angle = Mathf.LerpAngle(ship1.transform.rotation.eulerAngles.z, angley, rotateSpeed * Time.deltaTime);
+            ship1.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        }
+        if (p2rotation.x != 0f || p2rotation.y != 0f)
+        {
+            float angley = Mathf.Atan2(p2rotation.y, p2rotation.x) * Mathf.Rad2Deg;
+            angley = (angley + 360f) % 360f;
+            float angle = Mathf.LerpAngle(ship2.transform.rotation.eulerAngles.z, angley, rotateSpeed * Time.deltaTime);
+            ship2.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        }
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,68 +4,12 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour {
 
-    public bool player;
-
-    public float moveSpeed = 2f;
-    public float rotateSpeed = 5f;
+    public float speedMultiplier = 1f;
 
     public Rigidbody2D rb;
-
-    private Vector2 movement;
-    private float movHor;
-    private float movVer;
-    private float rotHor;
-    private float rotVer;
-
-    private void Start()
+    
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        //Position
-        if (player)
-        {
-            movHor = Input.GetAxisRaw("P1MovHor");
-            movVer = Input.GetAxisRaw("P1MovVer");
-        }
-        else
-        {
-            movHor = Input.GetAxisRaw("P2MovHor");
-            movVer = Input.GetAxisRaw("P2MovVer");
-        }
-
-        //Rotation
-        if (player)
-        {
-            rotHor = Input.GetAxisRaw("P1RotHor");
-            rotVer = Input.GetAxisRaw("P1RotVer");
-        }
-        else
-        {
-            rotHor = Input.GetAxisRaw("P2RotHor");
-            rotVer = Input.GetAxisRaw("P2RotVer");
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(movHor, movVer).normalized * moveSpeed;
-
-        if (rotHor != 0f || rotVer != 0f)
-        {
-            float angley = Mathf.Atan2(rotVer, rotHor) * Mathf.Rad2Deg;
-            angley = (angley + 360f) % 360f;
-            float angle = Mathf.LerpAngle(transform.rotation.eulerAngles.z, angley, rotateSpeed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-        }
-    }
-
-    private void OnGUI()
-    {
-        if (player) {
-            GUILayout.Label("movHor: " + movHor + " movVer: " + movVer + "\nrotHor: " + rotHor + " rotVer: " + rotVer);
-        }
     }
 }
