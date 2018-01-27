dc92eb4e885cde40607bfc46a24f32f7ea645a61
IKStreamIvo <ivostreaming@gmail.com>
2018-01-27 13:41:58 +0100

    Added more very interesting stuff

---
+++ Assets/Scripts/SelectedShip.cs
@@ -0,0 +1,16 @@
+﻿public static class SelectedShip
+{
+    private static int ship;
+
+    public static int Ship
+    {
+        get
+        {
+            return ship;
+        }
+        set
+        {
+            ship = value;
+        }
+    }
+}
